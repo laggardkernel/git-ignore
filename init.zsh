323597dc601688dd3e0999a7e69e1e4d2f0ce8bb
laggardkernel <laggardkernel@gmail.com>
2019-01-20 13:50:09 +0800

    Make thins work

--- init.zsh
+++ init.zsh
@@ -5,16 +5,19 @@
 #   laggardkernel <laggardkernel@gmail.com>
 #
 
+-gitignore-warn() { printf "%b[Warn]%b %s\n" '\e[0;33m' '\e[0m' "$@" >&2; }
+-gitignore-info() { printf "%b[Info]%b %s\n" '\e[0;32m' '\e[0m' "$@" >&2; }
+
 # git ignore generator
 export FORGIT_GI_REPO=~/.forgit/gi/repos/dvcs/gitignore # https://github.com/dvcs/gitignore.git
 export FORGIT_GI_SRC=$FORGIT_GI_REPO/templates
 
 -gitignore-update() {
   if [[ -d "$FORGIT_GI_REPO" ]]; then
-    forgit::info 'Updating gitignore repo...'
+    -gitignore-info 'Updating gitignore repo...'
     (cd $FORGIT_GI_REPO && git pull --no-rebase --ff) || return 1
   else
-    forgit::info 'Initializing gitignore repo...'
+    -gitignore-info 'Initializing gitignore repo...'
     git clone --depth=1 https://github.com/dvcs/gitignore.git "$FORGIT_GI_REPO"
   fi
 }
@@ -33,7 +36,7 @@ export FORGIT_GI_SRC=$FORGIT_GI_REPO/templates
   for item in "$@"; do
     filename=$(find -L "$FORGIT_GI_SRC" -type f \( -iname "${item}.gitignore" -o -iname "${item}.stack" \) -print -quit)
     if [[ -z "$filename" ]]; then
-      forgit::warn "No gitignore template found for '$item'." && continue
+      -gitignore-warn "No gitignore template found for '$item'." && continue
     elif [[ "$filename" == *.gitignore ]]; then
       header="${filename##*/}"
       header="${header%.gitignore}"
@@ -65,34 +68,34 @@ export FORGIT_GI_SRC=$FORGIT_GI_REPO/templates
 }
 
 gitignore() {
-  [ -d $FORGIT_GI_REPO ] || forgit::ignore::update
+  [ -d $FORGIT_GI_REPO ] || -gitignore-update
   local IFS cmd args options opt cat
   # https://github.com/wfxr/emoji-cli
   hash bat &>/dev/null && cat='bat -l gitignore --color=always --style=numbers,grid,header' || cat="cat"
   cmd="{ $cat $FORGIT_GI_SRC/{2}{.gitignore,.patch}; $cat $FORGIT_GI_SRC/{2}*.stack } 2>/dev/null"
   # shellcheck disable=SC2206,2207
-  IFS=$'\n' args=($@) && [[ $# -eq 0 ]] && args=($(forgit::ignore::list | nl -nrn -w4 -s'  ' |
-    forgit::fzf -m --preview="$cmd" --preview-window="right:70%" | awk '{print $2}'))
+  IFS=$'\n' args=($@) && [[ $# -eq 0 ]] && args=($(-gitignore-list | nl -nrn -w4 -s'  ' |
+    -gitignore-fzf -m --preview="$cmd" --preview-window="right:70%" | awk '{print $2}'))
   [ ${#args[@]} -eq 0 ] && return 1
   options=('(1) Output to stdout'
     '(2) Append to .gitignore'
     '(3) Overwrite .gitignore')
-  opt=$(printf '%s\n' "${options[@]}" | forgit::fzf +m | awk '{print $1}')
+  opt=$(printf '%s\n' "${options[@]}" | -gitignore-fzf +m | awk '{print $1}')
   # shellcheck disable=SC2068
   case "$opt" in
   '(1)')
     if hash bat &>/dev/null; then
-      forgit::ignore::get ${args[@]} | bat -l gitignore --style=numbers,grid
+      -gitignore-get ${args[@]} | bat -l gitignore --style=numbers,grid
     else
-      forgit::ignore::get ${args[@]}
+      -gitignore-get ${args[@]}
     fi
     ;;
   '(2)')
     ! [[ -e .gitignore ]] && touch .gitignore
-    forgit::ignore::get ${args[@]} >>.gitignore
+    -gitignore-get ${args[@]} >>.gitignore
     ;;
   '(3)')
-    forgit::ignore::get ${args[@]} >|.gitignore
+    -gitignore-get ${args[@]} >|.gitignore
     ;;
   esac
 }
